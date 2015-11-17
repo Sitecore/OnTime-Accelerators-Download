dda0274812cf6d676eeada14334cd5c0b3d0b6a4
Alex Povar <user.home.0000@gmail.com>
2015-11-17 14:42:33 +0200

    Added v3.0.0 release

---
+++ LatestRelease/OnTime_Helpers.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        OnTime Helpers
+// @description Provides a few helpful features to optimize work with OnTime. Developed by Alex Povar.
+// @author      Alex Povar
+// @downloadURL https://raw.githubusercontent.com/Sitecore/OnTime-Accelerators-Download/master/LatestRelease/OnTime_Helpers.user.js
+// @updateURL   https://raw.githubusercontent.com/Sitecore/OnTime-Accelerators-Download/master/LatestRelease/OnTime_Helpers.meta.js
+// @include     https://support.sitecore.net/client/*
+// @namespace   https://github.com/Sitecore/OnTime-Accelerators
+// @supportURL  https://github.com/Sitecore/OnTime-Accelerators/issues
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_xmlhttpRequest
+// @grant       GM_setClipboard
+// @grant       GM_getResourceURL
+// @resource    waitAnimation https://raw.githubusercontent.com/Sitecore/OnTime-Accelerators-Download/master/Resources/WaitAnimation.gif
+// @version     3.0.0
+// ==/UserScript==
