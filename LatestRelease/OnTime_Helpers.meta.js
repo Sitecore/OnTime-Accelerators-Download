272f23b210958c4baf807a81ab190a41f048c0e2
DK\mdo <mdo@sitecore.net>
2017-07-14 13:35:54 +0300

    Release 4.2.0

--- LatestRelease/OnTime_Helpers.meta.js
+++ LatestRelease/OnTime_Helpers.meta.js
@@ -5,13 +5,13 @@
 // @downloadURL https://raw.githubusercontent.com/Sitecore/OnTime-Accelerators-Download/master/LatestRelease/OnTime_Helpers.user.js
 // @updateURL   https://raw.githubusercontent.com/Sitecore/OnTime-Accelerators-Download/master/LatestRelease/OnTime_Helpers.meta.js
 // @include     https://support.sitecore.net/client/*
-// @namespace   https://github.com/Sitecore/OnTime-Accelerators
-// @supportURL  https://github.com/Sitecore/OnTime-Accelerators/issues
+// @namespace   https://github.com/SitecoreSupport/OnTime-Accelerators
+// @supportURL  https://github.com/SitecoreSupport/OnTime-Accelerators/issues
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
 // @grant       GM_setClipboard
 // @grant       GM_getResourceURL
 // @resource    waitAnimation https://raw.githubusercontent.com/Sitecore/OnTime-Accelerators-Download/master/Resources/WaitAnimation.gif
-// @version     4.1.0
+// @version     4.2.0
 // ==/UserScript==
