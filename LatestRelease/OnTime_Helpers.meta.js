770fd36c05a5d37c50102e4ad6c977b0cb21e541
DK\mdo <mdo@sitecore.net>
2019-03-19 12:04:50 -0400

    Release 4.6.2

--- LatestRelease/OnTime_Helpers.meta.js
+++ LatestRelease/OnTime_Helpers.meta.js
@@ -14,5 +14,5 @@
 // @grant       GM_setClipboard
 // @grant       GM_getResourceURL
 // @resource    waitAnimation https://raw.githubusercontent.com/Sitecore/OnTime-Accelerators-Download/master/Resources/WaitAnimation.gif
-// @version     4.6.1
+// @version     4.6.2
 // ==/UserScript==
